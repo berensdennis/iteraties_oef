ee65dae0e5249b38dfdb725742a115c011aefff1
berensdennis <56117546+berensdennis@users.noreply.github.com>
2019-11-26 11:52:32 +0100

    Update tafels_iteraties1.py

--- tafels_iteraties1.py
+++ tafels_iteraties1.py
@@ -1,3 +1,4 @@
+#met while
 from pcinput import getInteger
 
 num = getInteger( "Geef een nummer: " )
@@ -5,3 +6,9 @@ i = 1
 while i <= 10:
     print( i, "*", num, "=", i*num )
     i += 1
+#met for
+from pcinput import getInteger
+
+num = getInteger( "Geef een nummer: " )
+for i in range( 1, 11 ):
+    print( i, "*", num, "=", i*num )
